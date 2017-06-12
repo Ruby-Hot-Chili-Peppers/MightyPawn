07ffc9da67b50f9d764ffb41df2d8cfd288a2591
Smith Saintil <smith.saintil@gmail.com>
2017-06-12 20:38:40 +0000

    added piece model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612183110) do
+ActiveRecord::Schema.define(version: 20170612202836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20170612183110) do
     t.integer  "black_player_id"
     t.integer  "winner"
     t.integer  "turn"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.integer  "position_row"
+    t.integer  "position_column"
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.string   "color"
+    t.string   "type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
