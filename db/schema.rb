37d962f765c82885891881a6e1e6c3a40c48cc48
Smith Saintil <smith.saintil@gmail.com>
2017-06-12 18:41:07 +0000

    created game model and added belongs_to association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602014404) do
+ActiveRecord::Schema.define(version: 20170612183110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "white_player_id"
+    t.integer  "black_player_id"
+    t.integer  "winner"
+    t.integer  "turn"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
