3ca417f3d257fb6a7c5882c7270b27d2d2c72380
Chelsea Sandridge <chelsea.sandridge@gmail.com>
2017-06-26 13:28:41 +0000

    In pieces controller update moves

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -8,7 +8,7 @@ class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
     @game = Game.find(@piece.game_id)
-    @piece.update_attributes(position_row: params[:y_coord], position_column: params[:x_coord])
+    @piece.update_attributes(position_row: params[:y_coord], position_column: params[:x_coord], moves: @piece.moves + 1)
     redirect_to game_path(@game)
   end
 
