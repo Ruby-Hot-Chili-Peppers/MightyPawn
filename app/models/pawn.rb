f53792c3a01142e88dae16a50adf4ead8a9f8802
Chelsea Sandridge <chelsea.sandridge@gmail.com>
2017-06-27 14:57:38 +0000

    Added tests for moving a pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -5,7 +5,7 @@ class Pawn < Piece
 
   def valid_move?(new_row, new_column)
     #return false if you didnt move of if the move is obstructed
-    return false if position_row == new_row && position_column == new_column
+    return false if no_move?(new_row, new_column)
     return false if is_obstructed?(new_row, new_column)
 
     #------------------------------------WHITE PIECE LOGIC--------------------------------------------#
@@ -15,22 +15,22 @@ class Pawn < Piece
         #Check to see if there is a piece on the spot two ahead
         @two_ahead = Game.find(game_id).pieces.where(:position_row => position_row - 2, :position_column => position_column)
         #Only return true if we don't have a piece on the spot two ahead
-        return true if new_row == position_row - 2  && new_column == position_column && @two_ahead == nil
+        return true if new_row == position_row - 2  && new_column == position_column && @two_ahead.size == 0
       end
 
       #check to see if there is a piece on the spot one ahead, regardless of what move (including move = 0)
       @one_ahead = Game.find(game_id).pieces.where(:position_row => position_row - 1, :position_column => position_column)
       #Only return true if we don't have a piece on the spot straight ahead
-      return true if new_row == position_row - 1 && new_column = position_column && @one_ahead == nil
+      return true if new_row == position_row - 1 && new_column = position_column && @one_ahead.size == 0
       
       #we capture things diagonally, first figure out if a piece of the opposing color is diagonal
       #If the piece is the same color, it won't be a valid move
       @diag_right = Game.find(game_id).pieces.where(:color => 'black', :position_row => position_row - 1, :position_column => position_column + 1)
       @diag_left = Game.find(game_id).pieces.where(:color => 'black', :position_row => position_row - 1, :position_column => position_column - 1)
       #If there exists an opposing piece diagonally, we can move to the spot 
-      if @diag_right
+      if @diag_right.size != 0
         return true if new_row == position_row + 1 && new_column = position_column + 1 
-      elsif @diag_left
+      elsif @diag_left.size != 0
         return true if new_row == position_row + 1 && new_column = position_column - 1 
       end
     #------------------------------------BLACK PIECE LOGIC--------------------------------------------#
@@ -40,22 +40,22 @@ class Pawn < Piece
         #Check to see if there is a piece on the spot two ahead
         @two_ahead = Game.find(game_id).pieces.where(:position_row => position_row + 2, :position_column => position_column)
         #Only return true if we don't have a piece on the spot two ahead
-        return true if new_row == position_row + 2  && new_column == position_column && @two_ahead == nil
+        return true if new_row == position_row + 2  && new_column == position_column && @two_ahead.size == 0
       end 
 
       #check to see if there is a piece on the spot one ahead, regardless of what move (including move = 0)
       @one_ahead = Game.find(game_id).pieces.where(:position_row => position_row + 1, :position_column => position_column)
       #Only return true if we don't have a piece on the spot straight ahead
-      return true if new_row == position_row + 1  && new_column == position_column && @one_ahead == nil
+      return true if new_row == position_row + 1  && new_column == position_column && @one_ahead.size == 0 
       
       #we capture things diagonally, first figure out if a piece of the opposing color is diagonal
       #If the piece is the same color, it won't be a valid move
       @diag_right = Game.find(game_id).pieces.where(:color => 'white', :position_row => position_row + 1, :position_column => position_column + 1)
       @diag_left = Game.find(game_id).pieces.where(:color => 'white', :position_row => position_row + 1, :position_column => position_column - 1)
       #If there exists an opposing piece diagonally, we can move to the spot 
-      if @diag_right
+      if @diag_right.size != 0
         return true if new_row == position_row + 1 && new_column == position_column + 1
-      elsif @diag_left
+      elsif @diag_left.size != 0
         return true if new_row == position_row + 1 && new_column == position_column - 1
       end
     end
---
+++ spec/models/pawn_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Pawn, type: :model do
+  before do
+    @game = Game.create(id: 1, white_player_id: 1, black_player_id: 2)
+    @game.set_pieces_on_board
+    @game.set_default_turn
+    @pawn1 = Pawn.first #(position (1,0), color: black)
+    @pawn2 = Pawn.second #(position (1,1), color: black)
+    @pawn3 = Pawn.last #(position: (6,7), color: white)
+  end
+
+  describe "Pawn's movement" do
+    context 'no_move?' do 
+      it 'returns true for no move' do
+        expect(@pawn1.no_move?(1,0)).to be true
+      end
+
+      it 'returns false for move' do
+        expect(@pawn1.no_move?(1,1)).to be false
+      end
+    end
+
+    context 'is_obstructed?' do 
+      it 'returns true if we are obstructed' do
+        #put a pawn in the way
+        @pawn2.update_attributes(position_row: 2, position_column: 0)
+        expect(@pawn1.is_obstructed?(3,0)).to be true
+      end
+
+      it 'returns false if we are not obstructed' do
+        #take the pawn out of the way
+        @pawn2.update_attributes(position_row: 3, position_column: 2)
+        expect(@pawn1.is_obstructed?(3,0)).to be false
+      end
+    end
+
+     context 'valid_move?' do 
+      it 'allows us to move 2 spaces forward if we have not moved before' do
+        expect(@pawn1.valid_move?(3,0)).to be true
+      end
+
+      it 'does not allow us to move 2 spaces forward if we have already moved' do
+        #update number of moves
+        @pawn1.update_attributes(moves: @pawn1.moves + 1)
+        expect(@pawn1.valid_move?(3,0)).to be false
+      end
+
+      it 'allows us to move diagonally to capture a piece of a different color' do
+        #put pawn of opposing color in vertical position
+        @pawn3.update_attributes(position_row: 2, position_column: 1)
+        expect(@pawn1.valid_move?(2,1)).to be true
+      end
+
+      it 'does not allow us to move diagonally to capture a piece of the same color' do
+        #remove pawn of opposing color and add pawn of same color
+        @pawn3.update_attributes(position_row: 6, position_column: 7)
+        @pawn2.update_attributes(position_row: 2, position_column: 1)
+        expect(@pawn1.valid_move?(2,1)).to be false
+      end
+
+      it 'does not accept bogus moves!' do
+        expect(@pawn1.valid_move?(1,5)).to be false
+      end
+    end
+  end
+end
