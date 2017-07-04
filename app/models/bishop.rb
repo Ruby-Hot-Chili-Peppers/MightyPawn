5954424c1cdf5dfd1bff60a786b9305d67452e15
Chelsea Sandridge <chelsea.sandridge@gmail.com>
2017-07-04 13:22:26 +0000

    fixed such that all the former tests pass

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -5,7 +5,7 @@ class Bishop < Piece
   
   #should return false if rook unable to move
   def valid_move?(new_row, new_column)
-    return false if !super
+    return false if super == false
     #return false if bishop is blocked
     return false if is_obstructed?(new_row, new_column)
     #returns only true if moved in a diagonal
@@ -14,7 +14,6 @@ class Bishop < Piece
     return true if delta_row == delta_col 
     #If we don't have a valid move return false by default
     return false 
-    
   end  
   
   
