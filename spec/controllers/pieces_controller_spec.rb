5f019dd4d16dbc29b1373750f328c4e1a51ae42f
Edwin Flores <eflores003@gmail.com>
2017-07-08 04:00:18 +0000

    moved move_to! specs to model spec folder

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -37,12 +37,26 @@ RSpec.describe PiecesController, type: :controller do
       it "does not capture the piece if the desired position is occupied by the same color" do
         white_pawn1 = Pawn.second #located @ (1,1)
         #another piece of the same color exists in the position we want to move to
-        white_pawn2 = Pawn.third #(located @(1,2)
+        white_pawn2 = Pawn.third #(located @(3,2)
+        white_pawn2.update_attributes(position_row: 2, position_column: 1) 
+        puts "first puts"
+        p white_pawn2
+        puts "second"
+        p white_pawn1.valid_move?(2,1)
+        #white_pawn1.move_to!(2,1)
+        puts "third"
+        p white_pawn2
+        puts "fourth"
+        p Pawn.third
+        patch :update, params: {id: white_pawn1.id, y_coord: 2, x_coord: 1, moves: white_pawn1.moves + 1}
+        puts " "
+        white_pawn1.reload
+        p white_pawn1
 
-        expect{ white_pawn1.move_to!(1,2) }.to raise_error(RuntimeError)
-        
+        expect{ patch :update, 
+          params: {id: white_pawn1.id, y_coord: 2, x_coord: 1, moves: white_pawn1.moves + 1} }.to raise_error(RuntimeError)        
         #The piece in the desired position does not update its coordinates
-        expect([white_pawn2.position_row, white_pawn2.position_column]).to eq [1,2]
+        expect([white_pawn2.position_row, white_pawn2.position_column]).to eq [2,2]
       end
 
       it "captures the opposing team piece if the desired position is occupied by opposing team" do
@@ -52,6 +66,7 @@ RSpec.describe PiecesController, type: :controller do
 
         white_pawn1.move_to!(6,7)
 
+        black_pawn1.reload
         #the opposing piece should have its coordinates changed to nil, nil 
         expect([black_pawn1.position_row, black_pawn1.position_column]).to eq [nil,nil]
       end
