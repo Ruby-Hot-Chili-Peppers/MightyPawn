bdae417f3012a45c6904fe234f8241bf904a311c
Jeffrey Patajo <jpatajo@gmail.com>
2017-07-08 05:11:34 +0000

    Removed interstitial and added drag and drop

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -9,7 +9,6 @@ class PiecesController < ApplicationController
     @piece = Piece.find(params[:id])
     @game = Game.find(@piece.game_id)
     @piece.update_attributes(position_row: params[:y_coord], position_column: params[:x_coord], moves: @piece.moves + 1)
-    redirect_to game_path(@game)
+    render plain: 'updated!'
   end
-
 end
