089117a81963fcc5dd2027cf0ede9d85ef4ca62e
Edwin Flores <eflores003@gmail.com>
2017-07-22 23:33:54 +0000

    added rendering for 2nd player when 1st player makes a move. But its slow.

--- app/assets/javascripts/channels/pieces.js
+++ app/assets/javascripts/channels/pieces.js
@@ -3,8 +3,10 @@ When subscribed, can stream data that is broadcasted to the channel(piece contro
 
 App.pieces = App.cable.subscriptions.create('PiecesChannel', {
   received: function(data) {
-    return console.log(this.renderMessage(data));
-    /*return window.location.reload(true); can refresh windows through, only pass data and use jquery to append DOM */
+    console.log(this.renderMessage(data)); //check console of other player to see that data did pass through
+    alert("Other Player has made a move. Page will now reload");
+   return window.location.reload(true);  can refresh windows through, only pass data and use jquery to append DOM 
+    //return alert("Please Refresh page. Other player has moved");//
   },
 
   renderMessage: function(data) {
